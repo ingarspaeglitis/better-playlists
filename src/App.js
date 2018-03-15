267bb490fc1baeed642bc6a954a4ecf6da43ebcf
Ingars Paeglitis <ingars.paeglitis@yahoo.com>
2018-03-15 18:51:08 +0200

    Add custom components
    Added actual React components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+let defaultStyle = {
+  color: '#fff'
+}; 
+
+class Aggregate extends Component {
+  render() {
+    return (
+
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>      
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+
+      <div style={defaultStyle}>
+         <img/>
+         <input type='text' />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+         <img/>
+          <h3>Playlist Name</h3>
+          <ul>
+            <li>Song 1</li>
+            <li>Song 2</li>
+            <li>Song 3</li>            
+          </ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello Ingars</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+       
       </div>
     );
   }
